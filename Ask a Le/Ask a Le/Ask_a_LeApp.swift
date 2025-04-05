0516252305065ea6dcd6b25a20102018c4300708
Cong Le <cle155@csu.fullerton.edu>
2025-04-05 02:22:18 -0700

    initial setup

---
+++ Ask a Le/Ask a Le/Ask_a_LeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ask_a_LeApp.swift
+//  Ask a Le
+//
+//  Created by Cong Le on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Ask_a_LeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ask a Le/Ask a Le/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ask a Le
+//
+//  Created by Cong Le on 4/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
